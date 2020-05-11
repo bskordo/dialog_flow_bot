c5fdd747669db39c43f7c78ce2df12d3f84d2bce
bskordo <samat.study@gmail.com>
2020-05-11 23:33:03 +0600

    Changed get file function,and method creating reply resp list

--- create_intent.py
+++ create_intent.py
@@ -7,15 +7,14 @@ PROJECT_ID = os.environ['PROJECT_ID']
 def get_file_with_training_phrases():
     parser = argparse.ArgumentParser()
     parser.add_argument('training_phrases', help='Training phrases')
-    return parser.parse_args().training_phrases
+    return parser.parse_args()
 
 
 def create_intent(project_id, display_name, training_phrases_parts,
                   message_texts):
     intents_client = dialogflow.IntentsClient()
     parent = intents_client.project_agent_path(project_id)
-    reply_response = []
-    reply_response.append(message_texts)
+    reply_response =[message_texts]
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.types.Intent.TrainingPhrase.Part(
@@ -32,10 +31,11 @@ def create_intent(project_id, display_name, training_phrases_parts,
 
 
 def main():
-    training_phrases = get_file_with_training_phrases()
+    phrases = get_file_with_training_phrases()
+    training_phrases = phrases.training_phrases
     with open(training_phrases, "r") as phrases:
-        file_contents = json.load(phrases)
-        for display_name, dialog in file_contents.items():
+        file_subjects = json.load(phrases)
+        for display_name, dialog in file_subjects.items():
             create_intent(PROJECT_ID, display_name, dialog['questions'], dialog['answer'])
 
 
